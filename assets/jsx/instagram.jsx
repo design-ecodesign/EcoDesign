61597f03d550e74a3aea42f6a235c64d72c633f0
Krushil <krushilnaik96@gmail.com>
2020-10-10 07:20:09 -0400

    add share date to instagram posts

--- assets/jsx/instagram.jsx
+++ assets/jsx/instagram.jsx
@@ -3,13 +3,10 @@
 var jsonData;
 var instagramData = [];
 
-
 // Because Bootstrap's carousel wasn't good enough
 class InstagramPostCarousel extends React.Component {
 	render() {
 		let children = [];
-		// children.push();
-		// children.push();
 
 		// console.log(this.props.content);
 		for (const [i, data] of this.props.content.entries()) {
@@ -21,8 +18,7 @@ class InstagramPostCarousel extends React.Component {
 					</video>
 				);
 			} else {
-				// let url = data[0];
-				// console.log(url);
+				// console.log(data[0]);
 				children.push(
 					<img style={{display: i == this.props.activeView ? "inline-block" : "none"}} className="content" src={data[0]}/>
 				);
@@ -43,7 +39,6 @@ class InstagramPost extends React.Component {
 	}
 
 	switchView(direction) {
-		console.log(`Rendering at position ${this.state.current} of ${this.props.content.length}`);
 		if (direction == -1) {
 			if (this.state.current == 0) {
 				// console.log("r u dumb");
@@ -63,12 +58,14 @@ class InstagramPost extends React.Component {
 	}
 
 	render() {
+		// console.log(`Rendering at position ${this.state.current} of ${this.props.content.length-1}`);
+		// console.log(currentYear);
 		return (
 			<div className="instagram-post">
 				<img className="arrow next" onClick={() => this.switchView(1)} src="assets/images/icons/arrow.png"/>
 				<img className="arrow back" onClick={() => this.switchView(-1)} src="assets/images/icons/arrow.png"/>
 				<InstagramPostCarousel activeView={this.state.current} content={this.props.content}></InstagramPostCarousel>
-				<span className="instagram-post-date">PLACEHOLDER</span>
+				<span className="instagram-post-date">{this.props.postedOn[0].replace(`, ${(new Date()).getFullYear()}`, "")}</span>
 			</div>
 		);
 	}
@@ -91,17 +88,23 @@ class InstagramViewport extends React.Component {
 		if (this.readyState == 4 && this.status == 200) {
 			jsonData = JSON.parse(this.responseText);
 			jsonData.graphql.user.edge_owner_to_timeline_media.edges.slice(0,4).forEach(element => {
+				let postedOn = null;
 				let urls = element.node.edge_sidecar_to_children.edges.map(_e => {
+					// return;
 					if (_e.node.is_video) {
 						return [_e.node.video_url, true];
 					} else {
+						if (!postedOn) {
+							const rawCaption = _e.node.accessibility_caption;
+							postedOn = rawCaption.match(/[A-Z]{1}[a-z]* [0-9]{2}, [0-9]{4}/);
+							// console.log("posted on "+ postedOn);
+						}
 						return [_e.node.display_url, false];
 					}
 				});
-				// const postedOn = "";
 
 				instagramData.push(
-					<InstagramPost content={urls}></InstagramPost>
+					<InstagramPost content={urls} postedOn={postedOn}></InstagramPost>
 				);
 			});
 
